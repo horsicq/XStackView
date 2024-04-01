e3ab68a985031234f293981902137042f83b9828
horsicq <horsicq@gmail.com>
2024-04-01 20:11:30 +0200

    Add new file(s): 2024-04-01

---
+++ xstackview.cmake
@@ -0,0 +1,9 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+set(XSTACKVIEW_SOURCES
+    ${CMAKE_CURRENT_LIST_DIR}/xstackview.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xstackview.h
+    ${CMAKE_CURRENT_LIST_DIR}/xstackviewoptionswidget.ui
+    ${CMAKE_CURRENT_LIST_DIR}/xstackviewoptionswidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xstackviewoptionswidget.h
+)
